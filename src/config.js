342f70abd339d35b260082417a34815082793116
Noel <schenknoel@gmail.com>
2023-02-19 01:56:04 +0100

    add public config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  firebaseConfig: {
+    apiKey: 'AIzaSyB51fRc9MFhg3feZI80tairVX3JpCb4GQM',
+    authDomain: 'motorcycle-tracker-server.firebaseapp.com',
+    databaseURL:
+      'https://motorcycle-tracker-server-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'motorcycle-tracker-server',
+    storageBucket: 'motorcycle-tracker-server.appspot.com',
+    messagingSenderId: '196036965037',
+    appId: '1:196036965037:web:6ebf577dfb08c86d230c24',
+  },
+  auth: {
+    email: 'noel.schenk+read@outlook.com',
+    password: '0123456789',
+  },
+};
